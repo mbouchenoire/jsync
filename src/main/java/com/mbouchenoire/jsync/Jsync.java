52788a8d7b7dfc5e31a6db94e02cfa869a91ab2c
mbouchenoire <gitbouchenoire@gmail.com>
2016-12-29 23:06:47 +0100

    Apply static final fields naming conventions

--- src/main/java/com/mbouchenoire/jsync/Jsync.java
+++ src/main/java/com/mbouchenoire/jsync/Jsync.java
@@ -13,22 +13,22 @@ public final class Jsync {
     private static final long DEFAULT_TIMEOUT_SECONDS = 60;
     private static final ExecutorService PARALLEL_EXECUTOR = Executors.newCachedThreadPool();
 
-    private static final Parallel parallel = new Parallel(PARALLEL_EXECUTOR, DEFAULT_TIMEOUT_SECONDS);
-    private static final ForEach forEach = new ForEach(parallel);
+    private static final Parallel PARALLEL = new Parallel(PARALLEL_EXECUTOR, DEFAULT_TIMEOUT_SECONDS);
+    private static final ForEach FOREACH = new ForEach(PARALLEL);
 
     public static Map<Runnable, Throwable> parallel(Runnable... commands) {
-        return parallel.invoke(commands);
+        return PARALLEL.invoke(commands);
     }
 
     public static Map<Runnable, Throwable> parallel(Collection<Runnable> commands) {
-        return parallel.invoke(commands);
+        return PARALLEL.invoke(commands);
     }
 
     public static <T> Map<T, Throwable> forEach(T[] items, Consumer<T> consumer) {
-        return forEach.invoke(items, consumer);
+        return FOREACH.invoke(items, consumer);
     }
 
     public static <T> Map<T, Throwable> forEach(Collection<T> items, Consumer<T> consumer) {
-        return forEach.invoke(items, consumer);
+        return FOREACH.invoke(items, consumer);
     }
 }
