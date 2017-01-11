bdb2b097269beff0cabc9f7cabb4462ac7d378e6
mbouchenoire <gitbouchenoire@gmail.com>
2017-01-11 18:33:12 +0100

    Remove dependency to the Counter class in FilterTest

--- src/test/java/FilterTest.java
+++ src/test/java/FilterTest.java
@@ -15,19 +15,19 @@ import static org.junit.Assert.assertTrue;
  */
 public class FilterTest {
 
-    private static final List<Counter> counters = new ArrayList<Counter>(2);
+    private static final List<String> strings = new ArrayList<String>(2);
 
-    private static final Predicate<Counter> filterPositive = new Predicate<Counter>() {
-        public boolean test(Counter counter) {
-            return counter.getCount() >= 0;
+    private static final Predicate<String> filterNotNullOrEmpty = new Predicate<String>() {
+        public boolean test(String arg) {
+            return (arg != null && arg.length() > 0);
         }
     };
 
-    private static final Predicate<Counter> oneSecondSleepPredicate = new Predicate<Counter>() {
-        public boolean test(Counter counter) {
+    private static final Predicate<String> oneSecondSleepPredicate = new Predicate<String>() {
+        public boolean test(String arg) {
             try {
                 Thread.sleep(1000);
-                return filterPositive.test(counter);
+                return filterNotNullOrEmpty.test(arg);
             } catch (InterruptedException e) {
                 e.printStackTrace();
                 return false;
@@ -36,50 +36,50 @@ public class FilterTest {
     };
 
     static {
-        counters.add(new Counter(-5));
-        counters.add(new Counter(-1));
-        counters.add(new Counter(2));
-        counters.add(new Counter(4));
+        strings.add("hi");
+        strings.add(null);
+        strings.add("jsync");
+        strings.add("");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void nullItemsArrayShouldThrowException() {
-        Counter[] items = null;
-        Jsync.filter(items, filterPositive);
+        String[] strings = null;
+        Jsync.filter(strings, filterNotNullOrEmpty);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void nullItemsListShouldThrowException() {
-        List<Counter> items = null;
-        Jsync.filter(items, filterPositive);
+        List<String> strings = null;
+        Jsync.filter(strings, filterNotNullOrEmpty);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void nullPredicateShouldThrowException() {
-        Jsync.filter(counters, null);
+        Jsync.filter(strings, null);
     }
 
     @Test
     public void filterShouldFilter() {
-        final Collection<Counter> positiveCounters = Jsync.filter(counters, filterPositive);
-        final List<Counter> countersList = new ArrayList<Counter>(positiveCounters);
+        final Collection<String> notNullOrEmptyStrings = Jsync.filter(strings, filterNotNullOrEmpty);
+        final List<String> stringsList = new ArrayList<String>(notNullOrEmptyStrings);
 
-        assertEquals(2, countersList.size());
+        assertEquals(2, stringsList.size());
 
-        assertEquals(2, countersList.get(0).getCount());
-        assertEquals(4, countersList.get(1).getCount());
+        assertEquals("hi", stringsList.get(0));
+        assertEquals("jsync", stringsList.get(1));
     }
 
     @Test
     public void arrayOrCollectionShouldProduceSameResult() {
-        final Counter[] countersArray = new Counter[counters.size()];
+        final String[] countersArray = new String[strings.size()];
 
-        final Counter[] filteredCountersArray = Jsync.filter(counters.toArray(countersArray), filterPositive);
-        final Collection<Counter> filteredCountersCollection = Jsync.filter(counters, filterPositive);
-        final List<Counter> filteredCountersList = new ArrayList<Counter>(filteredCountersCollection);
+        final String[] filteredStringsArray = Jsync.filter(strings.toArray(countersArray), filterNotNullOrEmpty);
+        final Collection<String> filteredStringsCollection = Jsync.filter(strings, filterNotNullOrEmpty);
+        final List<String> filteredStringsList = new ArrayList<String>(filteredStringsCollection);
 
-        for(int i = 0; i < filteredCountersArray.length; i++) {
-            assertEquals(filteredCountersArray[i].getCount(), filteredCountersList.get(i).getCount());
+        for(int i = 0; i < filteredStringsArray.length; i++) {
+            assertEquals(filteredStringsArray[i], filteredStringsList.get(i));
         }
     }
 
@@ -88,7 +88,7 @@ public class FilterTest {
         final StopWatch stopWatch = new StopWatch();
 
         stopWatch.start();
-        Jsync.filter(counters, oneSecondSleepPredicate);
+        Jsync.filter(strings, oneSecondSleepPredicate);
         stopWatch.stop();
 
         System.out.println("4 * oneSecondSleepPredicate runned in " + stopWatch.getTime() + " millis");
