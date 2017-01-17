2bc92213819ae25c9a10bf60d103927e464d94bf
mbouchenoire <gitbouchenoire@gmail.com>
2017-01-17 20:37:38 +0100

    Add a configurable Jsync class to the api

---
+++ src/main/java/com/mbouchenoire/jsync/ConfigurableJsync.java
@@ -0,0 +1,65 @@
+package com.mbouchenoire.jsync;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+
+/**
+ * @author mbouchenoire
+ */
+public class ConfigurableJsync {
+
+    private final Parallel parallel;
+    private final ForEach forEach;
+    private final Mapper mapper;
+    private final Filter filter;
+
+    public ConfigurableJsync(long timeout, ExecutorService executorService) {
+        super();
+
+        if (timeout <= 0)
+            throw new IllegalArgumentException("timeout must be greater than 0");
+
+        if (executorService == null)
+            throw new IllegalArgumentException("executorService");
+
+        this.parallel = new Parallel(executorService, timeout);
+        this.forEach = new ForEach(parallel);
+        this.mapper =  new Mapper(parallel);
+        this.filter = new Filter(mapper);
+    }
+
+    public Set<ExecutionException> parallel(Runnable... commands) {
+        return this.parallel.invoke(commands);
+    }
+
+    public Set<ExecutionException> parallel(Collection<Runnable> commands) {
+        return this.parallel.invoke(commands);
+    }
+
+    public <T> void forEach(T[] items, Consumer<T> consumer) {
+        this.forEach.invoke(items, consumer);
+    }
+
+    public <T> void forEach(Collection<T> items, Consumer<T> consumer) {
+        this.forEach.invoke(items, consumer);
+    }
+
+    public <T, R> R[] map(T[] items, Function<T, R> function) {
+        return this.mapper.map(items, function);
+    }
+
+    public <T, R> List<R> map(List<T> items, Function<T, R> function) {
+        return this.mapper.map(items, function);
+    }
+
+    public <T> T[] filter(T[] items, Predicate<T> predicate) {
+        return this.filter.filter(items, predicate);
+    }
+
+    public <T> Collection<T> filter(Collection<T> items, Predicate<T> predicate) {
+        return this.filter.filter(items, predicate);
+    }
+}
--- src/main/java/com/mbouchenoire/jsync/Jsync.java
+++ src/main/java/com/mbouchenoire/jsync/Jsync.java
@@ -14,42 +14,36 @@ import java.util.concurrent.Executors;
 public final class Jsync {
 
     private static final long DEFAULT_TIMEOUT_SECONDS = 60;
-    private static final ExecutorService PARALLEL_EXECUTOR = Executors.newCachedThreadPool();
+    private static final ExecutorService DEFAULT_PARALLEL_EXECUTOR = Executors.newCachedThreadPool();
 
-    private static final Parallel PARALLEL = new Parallel(PARALLEL_EXECUTOR, DEFAULT_TIMEOUT_SECONDS);
-    private static final ForEach FOREACH = new ForEach(PARALLEL);
-    private static final Mapper MAPPER = new Mapper(PARALLEL);
-    private static final Filter FILTER = new Filter(MAPPER);
-
-    public static Set<ExecutionException> parallel(Runnable... commands) {
-        return  PARALLEL.invoke(commands);
-    }
+    private static final ConfigurableJsync INSTANCE
+            = new ConfigurableJsync(DEFAULT_TIMEOUT_SECONDS, DEFAULT_PARALLEL_EXECUTOR);
 
     public static Set<ExecutionException> parallel(Collection<Runnable> commands) {
-        return PARALLEL.invoke(commands);
+        return INSTANCE.parallel(commands);
     }
 
     public static <T> void forEach(T[] items, Consumer<T> consumer) {
-        FOREACH.invoke(items, consumer);
+        INSTANCE.forEach(items, consumer);
     }
 
     public static <T> void forEach(Collection<T> items, Consumer<T> consumer) {
-        FOREACH.invoke(items, consumer);
+        INSTANCE.forEach(items, consumer);
     }
 
     public static <T, R> R[] map(T[] items, Function<T, R> function) {
-        return MAPPER.map(items, function);
+        return INSTANCE.map(items, function);
     }
 
     public static <T, R> List<R> map(List<T> items, Function<T, R> function) {
-        return MAPPER.map(items, function);
+        return INSTANCE.map(items, function);
     }
 
     public static <T> T[] filter(T[] items, Predicate<T> predicate) {
-        return FILTER.filter(items, predicate);
+        return INSTANCE.filter(items, predicate);
     }
 
     public static <T> Collection<T> filter(Collection<T> items, Predicate<T> predicate) {
-        return FILTER.filter(items, predicate);
+        return INSTANCE.filter(items, predicate);
     }
 }
